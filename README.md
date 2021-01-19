02676e99072e98b51e6a9929d1c52abe08646d8a
nixgnauhcuy <nixgnauhcuy@gmail.com>
2021-01-19 17:51:43 +0800

    add UI files and some modifications

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # python_uart_tool
+
+## 简介
+
+**此项目是是一个简单的串口工具 Demo，实现串口的发送、接收，还有其他的小功能。该项目是基于 Python3 实现的。**
+
+## 内容
+
+**详细的项目内容可以移步到[我的博客](https://www.nixgnauhcuy.top)查看。🚀**
+
+## License
+
+[License](https://github.com/nixgnauhcuy/python_uart_tool/blob/main/LICENSE)
---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+from Ui_uart_tool_ui import Ui_MainWindow
+from PyQt5.QtWidgets import (QApplication, QMainWindow)
+
+
+class MyPyQT_Form(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+    my_pyqt_form = MyPyQT_Form()
+    my_pyqt_form.show()
+    sys.exit(app.exec_())
